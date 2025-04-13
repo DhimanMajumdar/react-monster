5fc369ddea9f75ea6fcf1872d3e26e107853dc1d
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-04-13 14:47:27 +0530

    usereducer done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
-    const [count,setCount]=useState(0);
-    const increment=()=>{
-        setCount(count+1);
-      }
-      const decrement=()=>{
-        setCount(count-1);
-      }
-    return (
-
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
     <div>
-        <h1>Counter</h1>
-        <h2>{count}</h2>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
-    )
-  
-}
+  );
+};
 
-export default Counter
+export default Counter;
